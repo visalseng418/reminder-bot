721c8035edea23284b4690e6a17a7f54246bd390
Chandaro <darozin97@gmail.com>
2026-02-24 11:19:30 +0700

    Resolved all merge conflicts

--- bot/session.js
+++ bot/session.js
@@ -1,12 +1,5 @@
-const sessions = {};
+// bot/session.js
+const { session } = require('telegraf');
 
-function getSession(chatId) {
-  if (!sessions[chatId]) sessions[chatId] = {};
-  return sessions[chatId];
-}
-
-function clearSession(chatId) {
-  delete sessions[chatId];
-}
-
-module.exports = { getSession, clearSession };
+// This allows ctx.session to work automatically in all handlers
+module.exports = session();
